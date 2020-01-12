9edbb30bebb5276e1fb63b623bd859873401c70b
raphir <rhicode@gmail.com>
2020-01-12 17:42:55 +0100

    #4 Added splashscreen

--- app/src/main/java/com/mobileapp/tripster/MainActivity.java
+++ app/src/main/java/com/mobileapp/tripster/MainActivity.java
@@ -2,13 +2,27 @@ package com.mobileapp.tripster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.buttonNavigate) Button button;
+
+
+    @OnClick(R.id.buttonNavigate) void navigateToNewActivity() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }

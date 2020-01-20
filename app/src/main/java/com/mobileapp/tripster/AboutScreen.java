20f4908fba11d35e2fbd743355bdea09cb3fec90
raphir <rhicode@gmail.com>
2020-01-20 15:03:16 +0100

    #1 minor adjustments to about screen activity

--- app/src/main/java/com/mobileapp/tripster/AboutScreen.java
+++ app/src/main/java/com/mobileapp/tripster/AboutScreen.java
@@ -2,13 +2,8 @@ package com.mobileapp.tripster;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 import butterknife.BindView;
 
@@ -23,6 +18,11 @@ public class AboutScreen extends AppCompatActivity {
         setContentView(R.layout.activity_about_screen);
 
         setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 
 }
